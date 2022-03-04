7508fd2d2543fdd07f25941633195f0d69f88c2e
chococake22 <fightest21@gmail.com>
2022-03-04 15:45:28 +0900

    스프링 mvc1 MVC 프레임워크 만들기

---
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.frontcontroller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// view에 보여질 수 있도록 렌더링하는 클래스
+public class MyView {
+
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+
+    }
+}
